7088e78225db0c314b541cc0e75fb1054852eb75
Majok-Bol <bolmajok001@gmail.com>
2025-03-10 13:34:13 +0300

    added border to text

--- lib/main.dart
+++ lib/main.dart
@@ -9,8 +9,26 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text('Rows and columns in Flutter')),
-        body: Center(child: Text('Rows and column widgets explained')),
+        appBar: AppBar(
+          title: Text(
+            'Rows and columns in Flutter',
+            style: TextStyle(fontSize: 30, color: Colors.redAccent),
+          ),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Container(
+            margin: EdgeInsets.all(10),
+            padding: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.black54),
+            ),
+            child: Text(
+              'Rows and column widgets explained',
+              style: TextStyle(fontSize: 20),
+            ),
+          ),
+        ),
       ),
     );
   }
